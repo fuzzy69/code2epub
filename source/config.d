03c88625c820d199ec20c1e8f73f8d2a02777c67
fuzzy69 <fuzzy@krsh>
2021-01-01 19:01:13 +0100

    Fixed build failure

--- source/config.d
+++ source/config.d
@@ -5,4 +5,4 @@ immutable(string) TITLE = "Code2Epub";
 immutable(string) TITLE_SHORT = "code2epub";
 immutable(string) DESCRIPTION = "Converts project source code to EPUB document";
 immutable(string) AUTHOR_NAME = "";
-// immutable(string) PROJECT_URL = "https://github.com/fuzzy69/code2epub";
+immutable(string) PROJECT_URL = "https://github.com/fuzzy69/code2epub";
